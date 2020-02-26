731f2792b0592619275ff9ad6dbfa886453c5f81
Matt Rix <matt@matt-rix.com>
2020-02-26 09:24:26 -0500

    Adding Arthur

---
+++ Assets/Hero.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Hero")]
+public class Hero : ScriptableObject
+{
+    public int age = 30;
+}
