ea258ad5b0397dcfde0d330c3b74123da07801e1
Matt Rix <matt@matt-rix.com>
2020-02-26 09:27:08 -0500

    Adding Fields

--- Assets/Hero.cs
+++ Assets/Hero.cs
@@ -6,4 +6,12 @@ using UnityEngine;
 public class Hero : ScriptableObject
 {
     public int age = 30;
+
+    public string bestFriend = "None";
+
+    public int hitPoints = 100; 
+
+    public List<int> levels = new List<int>(); 
+
+    public int baseDamage; //notice no default specified
 }
